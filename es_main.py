49657519f781874d8051c64d1c8351379f109a2e
FMoller <fredericomollerper@gmail.com>
2021-01-12 17:17:26 -0300

    functions and main program

---
+++ es_main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+N_IND = 20
+A = 100
+T_ITER = 1000
+
+AREA = np.zeros((A,A))
+RD =((0,0),(32,32))
+RC =((25,25),(75,75))
+
+
+
+
+
+####################
+#Placing           #
+####################
+#for i in range(N_IND):
+    
+
+
+#for i in range(T_ITER)
