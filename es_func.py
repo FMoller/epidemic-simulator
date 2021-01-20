c2ab5ebeb2ab3dbf8e19f1edcb7997ec80596f2b
FMoller <fredericomollerper@gmail.com>
2021-01-20 11:12:45 -0300

    Error correction in placement

--- es_func.py
+++ es_func.py
@@ -1,10 +1,11 @@
 
 
-def get_zones(A):
+def set_zones(A):
     rdl = int(((A*A)/20)**0.5)
     rll = A-int(A/3)
     rcl = int(A/3)
     RD = ((0,rdl),(0,rdl))
     RL = ((0,rll),(A-1,A-1))
     RC = ((rcl,rcl),(2*rcl,2*rcl))
+    
     
--- es_main.py
+++ es_main.py
@@ -1,24 +1,34 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import es_classes as ec
 
 N_IND = 20
-A = 100
+A = 10
 T_ITER = 1000
 
 AREA = np.zeros((A,A))
 RD =((0,0),(32,32))
 RC =((25,25),(75,75))
 
-
+popul = []
 
 
 
 ####################
 #Placing           #
 ####################
-#for i in range(N_IND):
+for i in range(N_IND):
+    pos_x = np.random.randint(A)
+    pos_y = np.random.randint(A)
+    popul.append(ec.Individual((pos_x,pos_y),(0,0),(0,0),(0,0)))
     
-
+#teste
+for i in popul:
+    AREA[i.pos[0],i.pos[1]]=1
 
 #for i in range(T_ITER)
+
+fig, ax = plt.subplots()
+ax.imshow(AREA)
+plt.show()
