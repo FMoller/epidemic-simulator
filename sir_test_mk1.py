b4c440fbe2b702a0016cfa4db17ad22b2b017abe
FMoller <fredericomollerper@gmail.com>
2021-02-26 20:38:21 -0300

    0.02.2

--- sir_test_mk1.py
+++ sir_test_mk1.py
@@ -15,15 +15,15 @@ from math import e
 
 step_size = 0.5
 S = 99999  #susceptible population
-SR2 = SR1 = S
+SR3 = SR2 = SR1 = S
 tau = 7*(1/step_size) #infection mean period
 beta = 3/tau  #infection producing contacts per unit time
 I = 1  #infected
-IR2 = IR1 = I
+IR3 = IR2 = IR1 = I
 R = 0  #recovered or dead
-RR2 = RR1 = R
+RR3 = RR2 = RR1 = R
 N=S+R+I
-NR2 = NR1 = N
+NR3 = NR2 = NR1 = N
 s_pop=[S]
 i_pop=[I]
 r_pop=[R]
@@ -33,8 +33,13 @@ rr1_pop=[RR1]
 sr2_pop=[SR2]
 ir2_pop=[IR2]
 rr2_pop=[RR2]
+sr3_pop=[SR3]
+ir3_pop=[IR3]
+rr3_pop=[RR3]
 period = int(100*(1/step_size))
 time_stamp = np.array(range(period+1))*step_size
+
+np.random.seed(1104)
 
 for t in range(period):
     tfac = np.random.normal(0,2)
@@ -43,39 +48,51 @@ for t in range(period):
     fac1 = beta*I*S/N
     fac1R1 = beta*IR1*SR1/NR1
     fac1R2 = beta2*IR2*SR2/NR2
+    fac1R3 = beta2*IR3*SR3/NR3
     fac2 = (I/tau)
     fac2R1 = (IR1/(tau+tfac))
     fac2R2 = (IR2/tau)
+    fac2R3 = (IR3/tau)
     ds = -fac1
     dsR1 = -fac1R1
     dsR2 = -fac1R2
+    dsR3 = -fac1R3
     di = fac1 - fac2
     diR1 = fac1R1 - fac2R1
     diR2 = fac1R2 - fac2R2
+    diR3 = fac1R3 - fac2R3
     dr = fac2
     drR1 = fac2R1
     drR2 = fac2R2
+    drR3 = fac2R3
     S+=ds
     SR1+=dsR1
     SR2+=dsR2
+    SR3+=dsR3
     I+=di
     IR1+=diR1
     IR2+=diR2
+    IR3+=diR3
     R+=dr
     RR1+=drR1
     RR2+=drR2
+    RR3+=drR3
     s_pop.append(S)
     sr1_pop.append(SR1)
     sr2_pop.append(SR2)
+    sr3_pop.append(SR3)
     i_pop.append(I)
     ir1_pop.append(IR1)
     ir2_pop.append(IR2)
+    ir3_pop.append(IR3)
     r_pop.append(R)
     rr1_pop.append(RR1)
     rr2_pop.append(RR2)
+    rr3_pop.append(RR3)
     N=S+R+I
     NR1=SR1+RR1+IR1
     NR2=SR2+RR2+IR2
+    NR3=SR3+RR3+IR3
 
 fig, ax = plt.subplots(2,2)
 ax[0,0].set_title("Normal")
@@ -90,6 +107,10 @@ ax[1,0].set_title("Var spread")
 ax[1,0].plot(time_stamp,sr2_pop)
 ax[1,0].plot(time_stamp,ir2_pop)
 ax[1,0].plot(time_stamp,rr2_pop)
+ax[1,1].set_title("Var double")
+ax[1,1].plot(time_stamp,sr3_pop)
+ax[1,1].plot(time_stamp,ir3_pop)
+ax[1,1].plot(time_stamp,rr3_pop)
 plt.show()
 
 
