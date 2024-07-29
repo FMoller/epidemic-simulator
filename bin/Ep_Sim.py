e08012275840dca07af33f3d9345ac74fef9083b
Frederico Möller <fredericomollerper@gmail.com>
2024-07-29 16:37:42 -0300

    Update Ep_Sim.py

--- bin/Ep_Sim.py
+++ bin/Ep_Sim.py
@@ -18,13 +18,29 @@ def main(argv):
     """
     Arguments:
     0 - Susceptivle pop , 1 - Infected pop, 2 - Recovered pop
-    3 - step size, 4 - infection mean period
+    3 - step size in fraction of a day, 4 - infection mean period in days
     5 - Infections produced by contacts in the infection mean period
-    
-    6 - Output file CSV
-    7 - Output file graph
+    6 - Total period in days
+    7 - Output file CSV
+    8 - Output file graph
+    9 - Seed
     """
     args = sys.argv
+    S = int(args[1])
+    I = int(args[2])
+    R = int(args[3])
+    step_size = int(args[4])
+    tau = int(args[5])*(1/step_size)
+    beta = int(args[6])/tau
+    period = int(int(args[7])*(1/step_size))
+     
+    N=S+R+I
+    s_pop=[S]
+    i_pop=[I]
+    r_pop=[R]
+    n_pop=[N]
+    
+    
 
 if __name__ == "__main__":
     main()
