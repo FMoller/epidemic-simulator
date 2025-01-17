08e78eb3e407e655726e859baae1cd900f9c75df
Frederico Möller <fredericomollerper@gmail.com>
2025-01-16 23:31:05 -0300

    Update gbes_classes.py

--- graph_based/gbes_classes.py
+++ graph_based/gbes_classes.py
@@ -1,4 +1,9 @@
 class node:
-    def __init__(self,base_id,name):
+    def __init__(self,base_id,name,node_type,pattern_matrix):
         self.base_id = base_id
         self.name = name
+        self.node_type = node_type
+        self.pattern_matrix = pattern_matrix
+
+class p_matrix:
+    pass
