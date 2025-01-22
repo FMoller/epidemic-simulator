8ba986f269892cd3df544e72a3b843eb25835ad7
Frederico Möller <fredericomollerper@gmail.com>
2025-01-22 00:00:14 -0300

    Update gbes_classes.py

--- graph_based/gbes_classes.py
+++ graph_based/gbes_classes.py
@@ -1,3 +1,12 @@
+"""
+Epidemic simulator classes
+
+This module contains the classes from the epidemic simulator
+"""
+
+__version__ = '0.1'
+__author__ = 'Frederico Moeller'
+
 class node:
     def __init__(self,base_id,name,node_type,pattern_matrix,connections,n2c):
         self.base_id = base_id
